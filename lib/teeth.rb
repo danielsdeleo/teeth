5dbf018a05943b4f31e99e9e43a8683384d32b8e
Daniel DeLeo <danielsdeleo@mac.com>
2009-03-29 21:41:56 -0600

    rescue errors when native extensions can't be loaded. Otherwise it is impossible to start completely clean and compile the extensions w/ rake tasks.

--- lib/teeth.rb
+++ lib/teeth.rb
@@ -1,5 +1,11 @@
-require "teeth/scan_apache_logs"
-require "teeth/scan_rails_logs"
+# encoding: utf-8
+begin
+  require "teeth/scan_apache_logs"
+  require "teeth/scan_rails_logs"
+rescue LoadError => e
+  STDERR.puts "WARNING: could not load extensions.  This is okay if you are creating them from\n" +
+              "source for the first time.  If that isn't the case, you're screwed"
+end
 $:.unshift File.dirname(__FILE__) + "/"
 require "scanner"
 require "scanner_definition"
